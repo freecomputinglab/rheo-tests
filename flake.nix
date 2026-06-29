bbd3da906b1da177435c8cb22e3b884599eb0f9b
Lachlan Kermode <lachie@ohrg.org>
2026-06-29 12:10:55 +0200

    Adds flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "rheo-tests - integration test suite for rheo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            echo "rheo-tests development environment loaded"
+            echo "Run tests with: RHEO_MANIFEST=../rheo/Cargo.toml cargo test --test harness"
+          '';
+        };
+      });
+}
